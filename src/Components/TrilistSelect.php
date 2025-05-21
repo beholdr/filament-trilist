c845131021f285bb60aa8f9d1ec4ee34d128819c
Alexander Shabunevich <loonar@gmail.com>
2025-05-21 10:35:48 +0300

    chore: explicit nullable types

--- src/Components/TrilistSelect.php
+++ src/Components/TrilistSelect.php
@@ -183,8 +183,8 @@ class TrilistSelect extends Field
     }
 
     public function relationship(
-        string | Closure $name = null,
-        Closure $modifyQueryUsing = null
+        string | Closure | null $name = null,
+        ?Closure $modifyQueryUsing = null
     ): static {
         $this->relationship = $name ?? $this->getName();
 
