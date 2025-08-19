b7c0abdcc535a8c03849d5847d9e5b25ccbdece1
Alexander Shabunevich <loonar@gmail.com>
2025-08-19 17:04:19 +0300

    feat(demo): add filament

--- demo/routes/web.php
+++ demo/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
