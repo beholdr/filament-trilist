67e7def262fc8bc92458968a5bb49c3001cdba39
Alexander Shabunevich <loonar@gmail.com>
2025-08-26 19:06:37 +0300

    chore: console message on build.js

--- bin/build.js
+++ bin/build.js
@@ -47,4 +47,6 @@ compile({
     ...defaultOptions,
     entryPoints: ['./resources/js/index.js'],
     outfile: './resources/dist/filament-trilist.js',
+}).then(() => {
+    console.log(`Build completed for filament-trilist.js`)
 })
